2ed51fda0c2ebb8c2b56abadcc7be2e77662e54c
Vladislav <105151334+Znakous@users.noreply.github.com>
2025-04-22 06:33:25 +0300

    started

--- lib/scheduler.h
+++ lib/scheduler.h
@@ -1,4 +1,20 @@
 #pragma once
 
+
+struct ret {};
 // TODO: Implement scheduler
-class TTaskScheduler {};
+class TTaskScheduler {
+public:
+    template<typename T, typename A, typename B>
+    ret add(T t, A a, B b){return ret();}
+
+
+    template<typename T, typename A>
+    ret getFutureResult(A a){return ret();}
+
+    template<typename T, typename A>
+    int getResult(A a){return int();}
+
+
+    void executeAll(){}
+};
